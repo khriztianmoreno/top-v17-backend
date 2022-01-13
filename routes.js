dcfa04c893c9a70e26e724ca1adec97d970b0e6b
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-01-13 10:14:33 -0500

    feat: Add upload endpoint

--- routes.js
+++ routes.js
@@ -3,12 +3,14 @@ const helloworld = require('./api/helloworld');
 const authLocal = require('./auth/local');
 const note = require('./api/note');
 const user = require('./api/user');
+const upload = require('./api/upload');
 
 // defining routes
 function routes(app) {
   app.use('/api/notes', note);
   app.use('/api/helloworld', helloworld);
   app.use('/api/users', user);
+  app.use('/api/uploads', upload);
 
   // auth routes
   app.use('/auth/local', authLocal);
