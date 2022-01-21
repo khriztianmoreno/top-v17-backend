e527ea34c3620af10b19040305a00780e007f796
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-01-21 09:39:06 -0500

    ci: update circleci test filter branch

--- api/helloworld/index.js
+++ api/helloworld/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ msg: 'Hello World!' });
 });
 
 module.exports = router;
